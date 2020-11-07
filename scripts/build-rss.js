3ad03f07583bd2ca4133efcb72b27318432db901
Wayne Dahlberg <waynefj40@gmail.com>
2020-11-06 20:19:12 -0700

    Update build-rss.js

--- scripts/build-rss.js
+++ scripts/build-rss.js
@@ -4,15 +4,15 @@ import getAllPostPreviews from '../src/getAllPostPreviews'
 
 const feed = new RSS({
   title: 'Blog – Wayne Dahlberg',
-  site_url: 'https://blog.waynedahlberg.com',
-  feed_url: 'https://blog.waynedahlberg.com/feed.xml',
+  site_url: 'https://blog.tailwindcss.com',
+  feed_url: 'https://blog.tailwindcss.com/feed.xml',
 })
 
 getAllPostPreviews().forEach(({ link, module: { meta } }) => {
   feed.item({
     title: meta.title,
     guid: link,
-    url: `https://blog.waynedahlberg.com${link}`,
+    url: `https://blog.tailwindcss.com${link}`,
     date: meta.date,
     description: meta.description,
     custom_elements: [].concat(meta.authors.map((author) => ({ author: [{ name: author.name }] }))),
