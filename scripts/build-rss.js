af37afe23618894142abc7ab1c32a67e75833e61
Wayne Dahlberg <waynefj40@gmail.com>
2020-11-06 20:25:20 -0700

    Update build-rss.js

--- scripts/build-rss.js
+++ scripts/build-rss.js
@@ -3,7 +3,7 @@ import RSS from 'rss'
 import getAllPostPreviews from '../src/getAllPostPreviews'
 
 const feed = new RSS({
-  title: 'Blog – Wayne Dahlberg',
+  title: 'Blog – Tailwind CSS',
   site_url: 'https://blog.tailwindcss.com',
   feed_url: 'https://blog.tailwindcss.com/feed.xml',
 })
