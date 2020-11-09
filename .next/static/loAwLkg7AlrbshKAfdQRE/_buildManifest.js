774c67376bda6f4cf57e6ed347ce081b12680fed
Wayne Dahlberg <waynefj40@gmail.com>
2020-11-08 21:06:04 -0700

    swap out twitter-card.jpg

---
+++ .next/static/loAwLkg7AlrbshKAfdQRE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/.introducing-verses":[a],"/110220-hello-world":[a],"/110520-figma-variants":[a],"/110720-swift-types":[a],"/110820-swiftui-basics":[a],"/index":[a]}}("static\u002Fchunks\u002F63565537ff974265952b17820f97e1be4a7b5198.e0365d641346d22b6eb3.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
