c95640c579eb392fa6620c55a1775134c6ffe071
Wayne Dahlberg <waynefj40@gmail.com>
2020-11-14 23:04:25 -0700

    update blog title responsiveness css

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,6 +1,6 @@
 export default function PageTitle({ children }) {
   return (
-    <h1 className="text-5xl leading-14 font-bold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+    <h1 className="text-3xl leading-14 font-bold text-gray-900 tracking-tight sm:text-5xl sm:leading-10 md:text-6xl md:leading-14">
       {children}
     </h1>
   )
