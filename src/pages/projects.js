1ac0909b23ef3e694991a7dfbcf128f17c52b1f3
Wayne Dahlberg <waynefj40@gmail.com>
2020-11-17 11:30:41 -0700

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -36,47 +36,7 @@ export default function Projects() {
             <div className="relative max-w-7xl mx-auto">
                 
                 <div className="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-3 lg:max-w-none">
-                <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-                    <div className="flex-shrink-0">
-                    <img className="h-48 w-full object-cover" src={r2d2} alt="" />
-                    </div>
-                    <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                    <div className="flex-1">
-                        <p className="text-sm leading-5 font-medium text-indigo-600">
-                        <a href="#" className="hover:underline">
-                            Electronics
-                        </a>
-                        </p>
-                        <a href="#" className="block">
-                        <h3 className="mt-2 text-xl leading-7 font-semibold text-gray-900">
-                            R2-D2 Astromech
-                        </h3>
-                        <p className="mt-3 text-base leading-6 text-gray-500">
-                            A journey to build a full-size, autonomous, aluminum replica of the movie prop. Bleep bloop.
-                        </p>
-                        </a>
-                    </div>
-                    <div className="mt-6 flex items-center">
-                        <div className="flex-shrink-0">
-                        <a href="#">
-                            <img className="h-10 w-10 rounded-full" src={avatar} alt="" />
-                        </a>
-                        </div>
-                        <div className="ml-3">
-                        <p className="text-sm leading-5 font-medium text-gray-900">
-                            <a href="#" className="hover:underline">
-                            Wayne
-                            </a>
-                        </p>
-                        <div className="flex text-sm leading-5 text-gray-500">
-                            <time datetime="2020-03-16">
-                            Coming soon
-                            </time>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
+                
                 <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
                     <div className="flex-shrink-0">
                     <img className="h-48 w-full object-cover" src={verses} alt="" />
@@ -133,6 +93,47 @@ export default function Projects() {
                         </h3>
                         <p className="mt-3 text-base leading-6 text-gray-500">
                             Thoughts and experiments building design systems in Figma.
+                        </p>
+                        </a>
+                    </div>
+                    <div className="mt-6 flex items-center">
+                        <div className="flex-shrink-0">
+                        <a href="#">
+                            <img className="h-10 w-10 rounded-full" src={avatar} alt="" />
+                        </a>
+                        </div>
+                        <div className="ml-3">
+                        <p className="text-sm leading-5 font-medium text-gray-900">
+                            <a href="#" className="hover:underline">
+                            Wayne
+                            </a>
+                        </p>
+                        <div className="flex text-sm leading-5 text-gray-500">
+                            <time datetime="2020-03-16">
+                            Coming soon
+                            </time>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                    <div className="flex-shrink-0">
+                    <img className="h-48 w-full object-cover" src={r2d2} alt="" />
+                    </div>
+                    <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                    <div className="flex-1">
+                        <p className="text-sm leading-5 font-medium text-indigo-600">
+                        <a href="#" className="hover:underline">
+                            Electronics
+                        </a>
+                        </p>
+                        <a href="#" className="block">
+                        <h3 className="mt-2 text-xl leading-7 font-semibold text-gray-900">
+                            R2-D2 Astromech
+                        </h3>
+                        <p className="mt-3 text-base leading-6 text-gray-500">
+                            A journey to build a full-size, autonomous, aluminum replica of the movie prop. Bleep bloop.
                         </p>
                         </a>
                     </div>
